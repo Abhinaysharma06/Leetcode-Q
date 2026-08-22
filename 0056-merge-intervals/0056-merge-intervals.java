1752469870f9b4a3b5ccfe639fc52ea89cd89173
Abhinaysharma06 <abhinaykumarsharma965@gmail.com>
2026-08-22 23:47:33 +0530

    Time: 8 ms (91.6%), Space: 48.7 MB (96%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
       Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-      List<List<Integer>>ans=new ArrayList<>();
+      List<int []>ans=new ArrayList<>();
       int s=intervals[0][0];
       int e=intervals[0][1];
        for(int i=1;i<intervals.length;i++){
@@ -9,20 +9,15 @@ class Solution {
             e=Math.max(e,intervals[i][1]);
            }
            else{
-            ans.add(Arrays.asList(s,e));
+            ans.add(new int[]{s,e});
            
            s=intervals[i][0];
            e=intervals[i][1];
            }
            }
-           ans.add(Arrays.asList(s,e));
+           ans.add(new int[]{s,e});
 
-           int [][]sol=new int[ans.size()][];
-           for(int i =0;i<ans.size();i++){
-            sol[i]=new int[2];
-            sol[i][0]= ans.get(i).get(0); 
-            sol[i][1] =ans.get(i).get(1);
-           }
-           return sol;
+           
+           return ans.toArray(new int[ans.size()][]);
     }
        }
